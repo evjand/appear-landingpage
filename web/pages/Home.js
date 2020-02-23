e61e0226b87887a835f00746dcb95ce5a8b0a446
Anders Evjen <evjand@AJ-Mac-Mini.local>
2020-02-23 14:30:18 +0100

    Add getInitialProps to Home.js

--- web/pages/Home.js
+++ web/pages/Home.js
@@ -1,4 +1,25 @@
 import Head from 'next/head'
+import groq from 'groq'
+import client from '../client'
+
+const pageQuery = groq`
+*[_type == "route" && slug.current == $slug][0]{
+  page-> {
+    ...,
+    content[] {
+      ...,
+      cta {
+        ...,
+        route->
+      },
+      ctas[] {
+        ...,
+        route->
+      }
+    }
+  }
+}
+`
 
 const Home = () => (
   <div className="container">
@@ -10,5 +31,44 @@ const Home = () => (
     <h1>Hallo</h1>
   </div>
 )
+
+Home.getInitialProps = async ({ query }) => {
+  const { slug } = query
+  if (!query) {
+    console.error('no query')
+    return
+  }
+  if (slug && slug !== '/') {
+    return client.fetch(pageQuery, { slug }).then(res => ({ ...res.page, slug }))
+  }
+
+  // Frontpage
+  if (slug && slug === '/') {
+    return client
+      .fetch(
+        groq`
+      *[_id == "global-config"][0]{
+        frontpage -> {
+          ...,
+          content[] {
+            ...,
+            cta {
+              ...,
+              route->
+            },
+            ctas[] {
+              ...,
+              route->
+            }
+          }
+        }
+      }
+    `
+      )
+      .then(res => ({ ...res.frontpage, slug }))
+  }
+
+  return null
+}
 
 export default Home
