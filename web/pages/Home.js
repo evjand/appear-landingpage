ce9e5c27cf3806eb34031b796ab1c5384c5d93c0
Anders Evjen <evjand@AJ-Mac-Mini.local>
2020-02-23 14:10:26 +0100

    Add Home page component

---
+++ web/pages/Home.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Appear</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <h1>Hallo</h1>
+  </div>
+)
+
+export default Home
